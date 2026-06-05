dfa419bd406a0d7eb44ea25f6de4b108e9f1de0b
bjmhe <hi@bjmhe.me>
2026-06-06 01:48:39 +0800

    chore: refactor autofix tasks in vite.config.ts and remove related scripts from package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,12 @@
 import { lib } from "@bjmhe/viteplus-preset";
 
-export default lib();
+export default lib(
+  {},
+  {
+    run: {
+      tasks: {
+        autofix: ["vpx automd", "vpx bjmhe fetch", "vp check --fix"],
+      },
+    },
+  },
+);
